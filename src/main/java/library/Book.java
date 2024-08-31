90b23d43a9cfcd5c3cb27218f532d75a38b9b8f9
vasupatel414 <patelvasu7562@gmail.com>
2024-08-31 22:29:44 +0530

    Implement the Book Class to pass the test

--- src/main/java/library/Book.java
+++ src/main/java/library/Book.java
@@ -7,6 +7,37 @@ public class Book {
     private int publicationYear;
     private boolean isAvailable;
 
-    
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true; // Initially, the book is available
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
 }
 
